4e3a981888800d56955d30f71d00aa7524023844
elsonreis77 <elsonreis084@gmail.com>
2025-11-14 21:34:28 -0300

    segundo commit

--- src/main/java/dobackaofront/model/Ficha.java
+++ src/main/java/dobackaofront/model/Ficha.java
@@ -5,4 +5,9 @@ import java.util.ArrayList;
 public class Ficha {
     private Aluno aluno;
     private ArrayList<Exercicios> exercicios;
+
+    public Ficha(Aluno aluno, ArrayList<Exercicios> exercicios) {
+        this.aluno = aluno;
+        this.exercicios = exercicios;
+    }
 }
