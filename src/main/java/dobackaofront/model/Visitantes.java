32751c786e414ee533c57f91f28389e59a5c81c1
elsonreis77 <elsonreis084@gmail.com>
2025-11-30 17:22:07 -0300

    Adiconando Visitantes

--- src/main/java/dobackaofront/model/Visitantes.java
+++ src/main/java/dobackaofront/model/Visitantes.java
@@ -6,5 +6,42 @@ public class Visitantes {
     private int cpf;
 
 
-    public Visitantes()
+    public Visitantes(String nome,String sexo, int cpf) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Visitantes{" +
+                "nome='" + nome + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", cpf=" + cpf +
+                '}';
+    }
 }
