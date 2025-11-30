52795fec2e478c5c6e7bd6455092e29b9af4ec59
elsonreis77 <elsonreis084@gmail.com>
2025-11-30 17:20:42 -0300

    testes

--- src/main/java/dobackaofront/model/Visitantes.java
+++ src/main/java/dobackaofront/model/Visitantes.java
@@ -2,7 +2,9 @@ package dobackaofront.model;
 
 public class Visitantes {
     private String nome;
+    private String sexo;
     private int cpf;
 
-    public Visitantes
+
+    public Visitantes()
 }
